e80e6f2cac777b0e00176ec66ebcde583a1d3ebd
gregoryls1 <contatogregory@gmail.com>
2018-08-21 11:17:27 -0300

    remoção dos componentes header e footer

--- gama/src/app/app.module.ts
+++ gama/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './pages/index/index.component';
 import { Error404Component } from './pages/error404/error404.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { MainNavComponent } from './shared/main-nav/main-nav.component';
 
 @NgModule({
@@ -15,8 +13,6 @@ import { MainNavComponent } from './shared/main-nav/main-nav.component';
     AppComponent,
     IndexComponent,
     Error404Component,
-    HeaderComponent,
-    FooterComponent,
     MainNavComponent
   ],
   imports: [
