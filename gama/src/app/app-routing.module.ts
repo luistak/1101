b4298fb879e26fcb78140aee91e9272d12c5082d
Luis Takahashi <luis.takahashi@liveecommerce.com.br>
2018-08-24 10:43:55 -0300

    Correção de conflito

--- gama/src/app/app-routing.module.ts
+++ gama/src/app/app-routing.module.ts
@@ -4,12 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
-<<<<<<< HEAD
 import { EventsComponent } from './pages/events/events.component';
-=======
 import { SignUpComponent } from './pages/signup/signup.component';
 import { UserAuthenticatedGuard } from './auth/user-authenticated.guard';
->>>>>>> a0cd63573c241e39381321ed3778e1a77e4970c9
 
 const routes: Routes = [
   {
@@ -17,13 +14,13 @@ const routes: Routes = [
     component: IndexComponent
   },
   {
-<<<<<<< HEAD
-    path: 'evento',
-    component: EventsComponent
-=======
     path: 'signup',
     component: SignUpComponent
->>>>>>> a0cd63573c241e39381321ed3778e1a77e4970c9
+  },
+  {
+    path: 'evento',
+    canActivate: [UserAuthenticatedGuard],
+    component: EventsComponent
   },
   {
     path: 'tarefas',
